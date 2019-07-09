47e1a336dc94193ac5b899101b26b6c24087c65e
RocketsFang <rockets.fang@gmail.com>
2019-07-09 12:01:16 +0800

    add generate db script

--- k8s_concepts_db/generatedata.sh
+++ k8s_concepts_db/generatedata.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+data_file=$1
+
+sigterm_handler() {
+  echo "Capture termination signal, and will backup the data file."
+  mv $data_file "$data_file"_$(date +"%F_%T")
+  exit $?
+}
+
+generate_data(){
+  COUNTER=1
+  echo "### this is the nodejs app file database" > $data_file
+  while [ true ]; do
+    echo "This is the " $COUNTER "th line." >> $data_file
+    let COUNTER=$COUNTER+1
+    sleep 5
+  done
+}
+
+##trap 'kill ${!}; sigterm_handler' SIGTERM
+trap sigterm_handler SIGTERM
+
+generate_data
