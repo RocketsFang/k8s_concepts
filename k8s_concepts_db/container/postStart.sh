d62e0ad34d32e66a330d0c99cb603b5aae5eca38
RocketsFang <rockets.fang@gmail.com>
2019-07-12 21:19:48 +0800

    a

--- k8s_concepts_db/container/postStart.sh
+++ k8s_concepts_db/container/postStart.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "The container[$(hostname)] postStart 'ExecAction' is executing ..."
+# need pod to configure with volume
+logFile=/opt/datastore/data/postStart.log
+echo "The container[$(hostname)] postStart 'ExecAction' is executing ..." 1> $logFile
 
 sigterm_handler() {
-  echo "Capture termination signal for preStart 'ExecAction', postStart shell will be terminated ..."
+  echo "Capture termination signal for preStart 'ExecAction', postStart shell will be terminated ..." 1>> $logFile
   COUNTER=1
   while [ true ]; do
-    echo "Time used to terminate - postStart shell: " $COUNTER " seconds..."
+    echo "Time used to terminate - postStart shell: " $COUNTER " seconds..." 1>> $logFile
     let COUNTER=$COUNTER+1
     sleep 1
   done
@@ -18,13 +20,13 @@ trap sigterm_handler SIGTERM
 
 duration=20
 while [ true ]; do
-    echo "Simulating works in postStart action duration[$duration] seconds: " $COUNTER " seconds..."
+    echo "Simulating works in postStart action duration[$duration] seconds: " $COUNTER " seconds..." 1>> $logFile
     let COUNTER=$COUNTER+1
     if [ $duration -le $COUNTER ]; then
-      "The works in postStart action is done ..."
+      "The works in postStart action is done ..." 1>> $logFile
       break;
     fi
     sleep 1
 done
 
-echo "The container[$(hostname)] postStart 'ExecAction' is ending ..."
+echo "The container[$(hostname)] postStart 'ExecAction' is ending ..." 1>> $logFile
--- k8s_concepts_db/container/preStop.sh
+++ k8s_concepts_db/container/preStop.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "The container[$(hostname)] preStop 'ExecAction' is executing ..."
+# need pod to configure with volume
+logFile=/opt/datastore/data/preStop.log
+echo "The container[$(hostname)] preStop 'ExecAction' is executing ..." 1> $logFile
 
 sigterm_handler() {
-  echo "Capture termination signal for preStop 'ExecAction', preStart shell will be terminated ..."
+  echo "Capture termination signal for preStop 'ExecAction', preStart shell will be terminated ..." 1>> $logFile
   COUNTER=1
   while [ true ]; do
-    echo "Time used to terminate - preStop shell: " $COUNTER " seconds..."
+    echo "Time used to terminate - preStop shell: " $COUNTER " seconds..." 1>> $logFile
     let COUNTER=$COUNTER+1
     sleep 1
   done
@@ -18,13 +20,13 @@ trap sigterm_handler SIGTERM
 
 duration=20
 while [ true ]; do
-    echo "Simulating works in preStop action duration[$duration] seconds: " $COUNTER " seconds..."
+    echo "Simulating works in preStop action duration[$duration] seconds: " $COUNTER " seconds..." 1>> $logFile
     let COUNTER=$COUNTER+1
     if [ $duration -le $COUNTER ]; then
-      "The works in preStop action is done ..."
+      echo "The works in preStop action is done ..." 1>> $logFile
       break;
     fi
     sleep 1
 done
 
-echo "The container[$(hostname)] preStop 'ExecAction' is ending ..."
+echo "The container[$(hostname)] preStop 'ExecAction' is ending ..." 1>> $logFile
